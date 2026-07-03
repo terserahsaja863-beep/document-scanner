3b71d4a814abd4289dcff4823a38380e0bf1b1e8
terserahsaja863-beep <terserahsaja863@gmail.com>
2026-07-03 15:42:26 +0700

    intialize commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Pro Scanner", layout="wide")
+
+st.title("📄 Pro Smart Document Scanner")
+st.write("Scanner + Enhancement + Download + Simple Session History")
+
+st.sidebar.success("Pilih menu di sidebar")
